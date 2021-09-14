b7188a43edddafb70c1edd7abe1f42c0e76838d0
1010592097ly <1010592097@qq.com>
2021-09-14 21:59:21 +0800

    ly-ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import LyButton from './components/button.vue'
 Vue.config.productionTip = false
 
+
+import './assets/fonts/iconfont.css'
+//全局注册button组件
+Vue.component(LyButton.name, LyButton)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
